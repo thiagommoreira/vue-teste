7732a5351382279d1a7a80a465e808b299586b3a
Thiago <thiagofoz@gmail.com>
2018-07-30 16:16:41 -0300

    Continuando adicionar feat.

--- src/components/quote-card/quote-card.component.vue
+++ src/components/quote-card/quote-card.component.vue
@@ -2,32 +2,40 @@
 <style src="./quote-card.component.css"></style>
 
 <script>
-    import QuoteIcon from '../quote-icon/quote-icon.component.vue';
-    import { Quote } from "./models/quote.model.js"
-    export default {
-        name: 'Quote-Card',
-        data() {
-            return {
-                newQuote: 'New Quote',
-                quote: {
-                    quote: '',
-                    author: ''
-                }
-            }
-        },
-        components: {
-            QuoteIcon
-        },
-        methods: {
-            getQuote: async function() {
-                const response = await this.$http.get('https://talaikis.com/api/quotes/random/')
-                const json = await response.json()
-                this.quote = new Quote(json)
-            }
-        },
-        created: function() {
-            this.getQuote();
-        }
+import QuoteIcon from "../quote-icon/quote-icon.component.vue";
+import { Quote } from "./models/quote.model.js";
+export default {
+  name: "Quote-Card",
+  data() {
+    return {
+      newQuote: "New Quote",
+      quote: {
+        quote: "",
+        author: ""
+      },
+      icons: [
+        { className: "fab fa-twitter-square fa-2x" },
+        { className: "fab fa-youtube-square fa-2x" }
+      ],
+      lastAuthor: ""
+    };
+  },
+  components: {
+    QuoteIcon
+  },
+  methods: {
+    getQuote: async function() {
+      this.lastAuthor = this.quote.author;
+      const response = await this.$http.get(
+        "https://talaikis.com/api/quotes/random/"
+      );
+      const json = await response.json();
+      this.quote = new Quote(json);
     }
+  },
+  created: function() {
+    this.getQuote();
+  }
+};
 </script>
 
